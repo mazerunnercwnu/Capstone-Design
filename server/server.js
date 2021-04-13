2f3742cc015de8ca3d19d66f3c5a333abae03293
mungmnb777 <mungmnb777@gmail.com>
2021-04-13 05:11:49 +0000

    g

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const port = 3001;
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const route = require('./routes/index');
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
 app.use(cors());
 app.use('/', route);
 
